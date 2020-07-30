aee124527cc1dc084d4ffe37ec8f20036f13bf49
Dmitriy Veretennikov <Dmitriy_Veretennikov@epam.com>
2020-07-30 10:26:19 +0300

    Add project files.

---
+++ HW.Otus/Controllers/HealthController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HW.Otus.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+       
+
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+  
+        [HttpGet]
+        public Object Health()
+        {
+            return new { Status = "OK" };
+        }
+    }
+}
